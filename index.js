d8b18630c0feddc969fa3e223d0ce7c47e388206
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-12 01:41:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function  (array) {
+  new_array = []
+  new_array.push(array[0])
+  new_array.push(array[1])
+  return new_array;
+};
+
+const returnLastTwoDrivers = function (array) {
+  new_array = []
+  new_array.push(array[array.length -2])
+  new_array.push(array[array.length -1])
+  return new_array;
+}
