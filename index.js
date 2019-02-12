e1006639fd10cd7ec4aff20ddb259b7fa7bab984
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-12 01:50:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,4 +22,6 @@ const createFareMultiplier = function(n) {
   }
 }
 
-const fareDoubler = createFareMultiplier(2)
+const fareDoubler = createFareMultiplier(2);
+
+const fareDoubler = createFareMultiplier(3);
